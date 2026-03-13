4c5ff38329a9d033cd8ddd07fc111e750f1164b3
starmap789 <starmap789@gmail.com>
2026-03-13 14:05:20 -0400

    Initial commit - no changes

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       mason                                                        #
+# 	Created:      3/13/2026, 1:59:27 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
